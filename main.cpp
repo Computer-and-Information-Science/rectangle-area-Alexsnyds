2e9dc9ac62a839f01174bd0ef8af4c2e5f499243
Alexsnyds <alexander.snyder@student.northampton.edu>
2026-01-31 19:18:22 +0000

    I coded the rectangle

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  const double length = 7.2;
-  const double width = 1.3;
+int main() { 
+  double length = 7.2;
+  double width = 1.3;   
+ 
+  
 
+  
+  cout << "rectangle properties:" << endl;
+       
 
+  double perimeter = 2 * (length + width);
+  double area = length * width;
+
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << " Perimeter = " << perimeter << endl;
+  cout << " Area = " << area << endl;
 }
